0308f17f5af41e22733bce74ed142578544835aa
Mohit Panjwani <mohit.panjvani@gmail.com>
2023-05-02 12:24:54 +0530

    add favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,54 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
     title: 'Crater Invoice - Open Source Documentation',
     description:
-    'Crater is an open-source app made in Laravel, VueJS & React Native that helps you track expenses, payments & create professional invoices & estimates',
+        'Crater is an open-source app made in Laravel, VueJS & React Native that helps you track expenses, payments & create professional invoices & estimates',
+    head: [
+        [
+        'link',
+        {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: '/images/favicons/apple-touch-icon.png',
+        },
+        ],
+        [
+        'link',
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/images/favicons/favicon-32x32.png',
+        },
+        ],
+        [
+        'link',
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            href: '/images/favicons/favicon-16x16.png',
+        },
+        ],
+        ['link', { rel: 'manifest', href: '/images/favicons/site.webmanifest' }],
+        [
+        'link',
+        {
+            rel: 'mask-icon',
+            href: '/images/favicons/safari-pinned-tab.svg',
+            color: '#5851d8',
+        },
+        ],
+        ['link', { rel: 'shortcut icon', href: '/images/favicons/favicon.ico' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+        [
+        'meta',
+        {
+            name: 'msapplication-config',
+            content: '/images/favicons/browserconfig.xml',
+        },
+        ],
+        ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ],    
     themeConfig: {
         siteTitle: false,
         logo: '/images/crater-logo.png',
